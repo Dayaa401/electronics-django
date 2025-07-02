eb072ec3e27fec4bf16e3c304d6a48d989e01e1a
ALLAD DAYANAND <alladadayanad@gmail.com>
2025-07-02 19:14:54 +0530

    Configured for deployment: updated wsgi, settings, added vercel.json, and installed whitenoise

--- mypro/settings.py
+++ mypro/settings.py
@@ -26,12 +26,14 @@ SECRET_KEY = 'django-insecure-h&hbgl0sbr779qd#b^(ddll@%_(9&118-u6!2ofe87%#*8%!m=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_FILES=[STATIC_DIR]
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
