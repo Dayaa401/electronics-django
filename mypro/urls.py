a323e33ad3da65f7951e3d8796acb100a1975e17
ALLAD DAYANAND <alladadayanad@gmail.com>
2025-07-10 22:00:13 +0530

    Fix re_path and serve media files on Render

--- mypro/urls.py
+++ mypro/urls.py
@@ -15,25 +15,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path  # ✅ Include re_path here
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve  # ✅ Needed to serve media files when DEBUG=False
 from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home),
-    path('signup/',views.signup,name='signup'),
-    path('login/',views.login,name='login'),
-    # path('theme/',views.theme,name='theme')
+    path('', views.home),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
     path('add-to-cart/', views.add_to_cart),
     path('cart/', views.cart),
     path('checkout/', views.checkout),
-
 ]
+
+# Serve static and media files
 if settings.DEBUG:
-    urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
     urlpatterns += [
         re_path(r'^media/(?P<path>.*)$', serve, {
